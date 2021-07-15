9f2c610afcf6018d5534b1bfdb4d7dd0a3d44545
ZLILDANIELPC <d4444i@gmail.com>
2021-07-15 09:20:20 +0300

    get and post function- shortcut

--- src/utils.js
+++ src/utils.js
@@ -1,7 +1,21 @@
   
 export function getChatUsersList(chat, usersContext) {
-    return chat?.userIds.map(user => {
-      let fullUser = usersContext.allUsers[user._id] || {};
+    return chat?.userIds.map(userId => {
+      let fullUser = usersContext.allUsers[userId] || {};
       return fullUser.userName;
     }).join(', ');
   }
+
+  export let get = (route) => fetch(`http://localhost:8080/api/${route}`, {
+      credentials: 'include',
+      mode: 'cors',
+  }).then(res => res.json())
+
+  export let post = (route, body) => fetch(`http://localhost:8080/api/${route}`, {
+      method: 'POST',
+      headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(body)
+  });
